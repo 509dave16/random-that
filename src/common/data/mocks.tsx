4272332adcc959d20d30d288e4a46113c6aadf91
David Fall <davidfall@rocketmail.com>
2018-07-05 19:23:26 -0700

    RT: Fix mock item data

--- src/common/data/mocks.tsx
+++ src/common/data/mocks.tsx
@@ -6,8 +6,8 @@ export const lists = [
 export const items = [
 	{ id: '1', list_id: '1', name: 'vacumming' },
 	{ id: '2', list_id: '2', name: 'hot dogs'},
-	{ id: '1', list_id: '1', name: 'mowing' },
-	{ id: '2', list_id: '2', name: 'hamburgers'},
-	{ id: '1', list_id: '1', name: 'laundry' },
-	{ id: '2', list_id: '2', name: 'maccaroni'}
+	{ id: '3', list_id: '1', name: 'mowing' },
+	{ id: '4', list_id: '2', name: 'hamburgers'},
+	{ id: '5', list_id: '1', name: 'laundry' },
+	{ id: '6', list_id: '2', name: 'maccaroni'}
 ];
