e1d6f10f924a8f40f7b2b1fc57ffea3507db7c34
David Fall <davidfall@rocketmail.com>
2018-07-06 14:21:22 -0700

    RT: Shadow dom example

--- src/components/pages/lists.page.tsx
+++ src/components/pages/lists.page.tsx
@@ -72,6 +72,15 @@ export default class ListsPage extends BaseComponent<Props, State> {
 		const addListClassNames = classNames('m-t-sm', 'button', 'is-info', {'is-hidden': nextState.addListIsHidden});
 		const createListClassNames = classNames('button', 'is-success', 'm-r-sm', { isLoading: nextState.waiting });
 
+		/* Shadow DOM Example
+		<style style=">
+				ion-icon svg,svg { color: red; }
+			</style>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="
+			height: 20px;
+			width: 20px;
+		"><path d="M294.1 256L167 129c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.3 34 0L345 239c9.1 9.1 9.3 23.7.7 33.1L201.1 417c-4.7 4.7-10.9 7-17 7s-12.3-2.3-17-7c-9.4-9.4-9.4-24.6 0-33.9l127-127.1z"></path></svg>
+		*/
 		return (
 			<PageComponent history={this.props.history} headerOptions={{ title: 'Lists'}}>
 				<div class="list">
