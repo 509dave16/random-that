8babbb98a7a1ad282ca96dcfc6390a07ec36d8b4
David Fall <davidfall@rocketmail.com>
2018-07-07 18:00:23 -0700

    Firebase setup, no need for gzip on that service

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,15 @@
 const merge = require('webpack-merge');
 const common= require('./webpack.common');
+const CompressionPlugin = require('compression-webpack-plugin');
 module.exports = merge(common, {
 	mode: 'production',
-	devtool: "source-map",
+	plugins: [
+		// new CompressionPlugin({
+		// 	asset: "[path].gz[query]",
+		// 	algorithm: "gzip",
+		// 	test: /\.js$|\.css$|\.html$/,
+		// 	threshold: 10240,
+		// 	minRatio: 0.8
+		// })
+	]
 });
